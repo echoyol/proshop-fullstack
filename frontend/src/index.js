10233c1317efeb706da84f9cba918076566d453a
Sunny <sunsunsunny20@gmail.com>
2024-01-24 16:29:31 +0800

    add shipping screen and payment screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,9 @@ import store from './store'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PrivateRoute from './components/PrivateRoute'
+import PaymentScreen from './screens/PaymentScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -44,6 +47,20 @@ const router = createBrowserRouter(
         path='/register'
         element={<RegisterScreen />}
       />
+
+      <Route
+        path=''
+        element={<PrivateRoute />}
+      >
+        <Route
+          path='/shipping'
+          element={<ShippingScreen />}
+        />
+        <Route
+          path='/payment'
+          element={<PaymentScreen />}
+        />
+      </Route>
     </Route>
   )
 )
