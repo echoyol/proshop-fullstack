f2eea6498fa03db016c27c761b7d9b6de6cf310b
Sunny <sunsunsunny20@gmail.com>
2024-02-08 13:07:20 +0800

    add search function

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import { useLogoutMutation } from '../slices/usersApiSlice'
 import { logout } from '../slices/authSlice'
+import SearchBox from './SearchBox'
 function Header() {
   const { cartItems } = useSelector((state) => state.cart)
   const { userInfo } = useSelector((state) => state.auth)
@@ -45,6 +46,7 @@ function Header() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
+              <SearchBox />
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart />
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,16 +3,27 @@ import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Paginate from '../components/Paginate'
 
 function HomeScreen() {
-  const { pageNumber } = useParams()
+  const { pageNumber, keyword } = useParams()
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
-  console.log(data)
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  })
+
   return (
     <>
+      {keyword && (
+        <Link
+          to='/'
+          className='btn btn-light mb-4'
+        >
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -38,6 +49,7 @@ function HomeScreen() {
           <Paginate
             pages={data.pages}
             page={data.page}
+            keyword={keyword ? keyword : ''}
           />
         </>
       )}
