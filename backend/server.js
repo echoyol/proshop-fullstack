b3ed6c21b2e94020a9ffaf10fda4b4825157bfe0
Sunny <sunsunsunny20@gmail.com>
2024-02-17 15:42:57 +0800

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,23 @@
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-import connectDB from './config/db.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js'
-import cookieParser from 'cookie-parser'
-dotenv.config()
-
-connectDB() //connect to MongoDB
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const port = process.env.PORT || 5000
 
+connectDB()
+
 const app = express()
 
-// app.get('/', (req, res) => {
-//   res.send('API is running')
-// })
-//Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-//Cookie parser middleware
 app.use(cookieParser())
 
 app.use('/api/products', productRoutes)
@@ -35,24 +29,25 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
 
-const __dirname = path.resolve() //set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
 if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static('/var/data/uploads'))
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
   app.get('/', (req, res) => {
-    res.send('API is running')
+    res.send('API is running....')
   })
 }
 
-//Error handling middleware
 app.use(notFound)
-
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+)
